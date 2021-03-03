425a483deda4dec43e847657cce22ae43310c4ad
Isamu Ito <isamsito@gmail.com>
2021-03-03 22:09:12 +0000

    order descending

--- 1_queries/most_visited_cities.sql
+++ 1_queries/most_visited_cities.sql
@@ -2,4 +2,4 @@ SELECT city, count(reservations.*) as total_reservations
 FROM properties
 JOIN reservations ON properties.id = reservations.property_id
 GROUP BY city
-ORDER BY total_reservations;
+ORDER BY total_reservations DESC;
